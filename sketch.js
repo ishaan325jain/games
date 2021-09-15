7284a38cbdee17d5a64be5a5f09d48fdb80e53f3
ishaan325jain <73381737+ishaan325jain@users.noreply.github.com>
2021-09-15 13:11:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var spacebar,gamestate , stage;
+var mario   , marioIMG
+var donkey  , donkeyIMG
+var barrel  , barrelIMG
+var	princess, princessIMG
+var	Platform,platformIMG
+var Ladder  , ladderIMG 
+var	healthMiniMario 
+var	name
+var rank
+var	playerNumber
+var	highScore
+var Bonus
+var Screen1IMG
+var	Screen2IMG
+var	Screen3IMG
+var	Screen4IMG
+var	Howhighdonkeykong
+
+
+function preload(){
+}
+
+function setup(){
+  
+}
+
+
+function draw(){
+}
